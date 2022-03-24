33231ab1b5e15a5ac232ecc792ad24e277306f4e
Jonas Andersen <jonas@anrsn.me>
2022-03-24 10:41:17 +0100

    Test sampling rate of RAPL

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -4,13 +4,33 @@ class Program
 {
     private const string FILE_PATH = "/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj";
 
-    private static Decimal read_rapl_value() {
+    private static Int64 read_rapl_value() {
         string raw_value = System.IO.File.ReadAllText(FILE_PATH);
 
-        return Decimal.Parse(raw_value);
+        return Int64.Parse(raw_value);
     }
     static void Main(string[] args){
-        Console.WriteLine("Iteration;RAPL-Value;Old-RAPL;New-RAPL;Ticks");
+        int measurement_cnt = 200;
+        long[] measurements = new long[measurement_cnt];
+        for (int i = 0; i < measurement_cnt; i++)
+        {
+            bool changed = false;
+            var inital_value = read_rapl_value();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            while(!changed) {
+                var new_value = read_rapl_value();
+                changed = new_value != inital_value;
+            }
+            watch.Stop();
+            measurements[i] = watch.ElapsedTicks;
+        }
+        System.Console.WriteLine(measurements[0]);
+        Array.Sort(measurements);
+        foreach (var item in measurements)
+        {
+            System.Console.WriteLine(item);
+        }
+/*         Console.WriteLine("Iteration;RAPL-Value;Old-RAPL;New-RAPL;Ticks");
         for(int count = 0; count < 25; count++){
             Decimal before_value = read_rapl_value();
             var watch = System.Diagnostics.Stopwatch.StartNew();
@@ -21,7 +41,7 @@ class Program
             watch.Stop();
             Console.WriteLine("{0};{1};{2};{3};{4}", count, new_value-before_value, before_value, new_value, watch.ElapsedTicks);
         }
-        
+         */
         /* int fibval = Int32.Parse(args[0]);
         Console.WriteLine(FibLibrary.GetFib(fibval));
         foreach(var x in args){
