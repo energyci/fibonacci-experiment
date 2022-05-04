078bac9bc157f3fe9e47480005ac2beb4b3729ef
Jonas Andersen <jonas@anrsn.me>
2022-05-04 11:42:30 +0200

    Fib unit tests, time based now

--- FibLibraryTest/IterativeTestMethodAttribute.cs
+++ FibLibraryTest/IterativeTestMethodAttribute.cs
@@ -5,106 +5,54 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 
-namespace StringLibraryTest
-{
-
-    public class RAPLTestResult : TestResult {
-
-        public Decimal RAPLValue { get; }
-        public RAPLTestResult(Decimal RAPLValue, TestResult data) : base(){
-            this.RAPLValue = RAPLValue;
-            this.DisplayName = data.DisplayName;
-            this.Outcome = data.Outcome;
-            this.TestFailureException = data.TestFailureException;
-            this.LogOutput = data.LogOutput;
-            this.LogError = data.LogError;
-            this.DebugTrace = data.DebugTrace;
-            this.TestContextMessages = data.TestContextMessages;
-            this.ExecutionId = data.ExecutionId;
-            this.ParentExecId = data.ParentExecId;
-            this.InnerResultsCount = data.InnerResultsCount;
-            this.Duration = data.Duration;
-            this.DatarowIndex = data.DatarowIndex;
-            this.ReturnValue = data.ReturnValue;
-            this.ResultFiles = data.ResultFiles;
-
-        }
-    }
-
-    public class RAPLPackage { 
-
-        public String Name { get; }
-        public Decimal Counter { get; }
-        public Dictionary<String, Decimal> ZoneValues { get; }
-        private void ReadRAPLZone(String file_path) {
-            String name = System.IO.File.ReadAllText(Path.Join(file_path, "name"));
-            Decimal value = Decimal.Parse(System.IO.File.ReadAllText(Path.Join(file_path, "energy_uj")));
-            ZoneValues[name] = value;
-        }
-
-        // file_path = "/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0"
-
-        public RAPLPackage(String file_path) {
-            ZoneValues = new Dictionary<string, decimal>();
-            var x = Directory.GetDirectories(file_path, "intel-rapl*");
-            foreach(var y in x ) {
-                ReadRAPLZone(y);
-            }
-            Name = System.IO.File.ReadAllText(Path.Join(file_path, "name"));
-            Counter = Decimal.Parse(System.IO.File.ReadAllText(Path.Join(file_path, "energy_uj")));
-
-            
-            Console.WriteLine("Name: {0}, Energy_uj: {1}", Name, Counter);
-            foreach(var item in ZoneValues) {
-                Console.WriteLine("  Name: {0}, Value: {1}", item.Key, item.Value);
-            }
-        }
-    }
+namespace StringLibraryTest {
 
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class IterativeTestMethodAttribute : TestMethodAttribute
-    {
+    public class IterativeTestMethodAttribute : TestMethodAttribute {
         private int stabilityThreshold;
-        private const string FILE_PATH = "/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj";
+        private const string FILE_PATH = "/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:0/energy_uj";
 
-        private Decimal read_rapl_value() {
-            string raw_value = System.IO.File.ReadAllText(FILE_PATH);
-
-            return Decimal.Parse(raw_value);
+        private string read_rapl_value() {
+            return System.IO.File.ReadAllText(FILE_PATH);
         }
 
-        public IterativeTestMethodAttribute(int stabilityThreshold)
-        {
+        public IterativeTestMethodAttribute(int stabilityThreshold) {
             this.stabilityThreshold = stabilityThreshold;
         }
 
-        public override TestResult[] Execute(ITestMethod testMethod)
-        {
-            var results = new List<RAPLTestResult>();
-            Console.WriteLine("Iteration;RAPL-Value;Old-RAPL;New-RAPL;Ticks");
-            for (int count = 0; count < this.stabilityThreshold; count++)
-            {
-                Decimal before_value = read_rapl_value();
-                var watch = System.Diagnostics.Stopwatch.StartNew();
+        public override TestResult[] Execute(ITestMethod testMethod) {
+            var results = new List<TestResult>();
+            var tuples = new List<(decimal, long)>();
+            var begin_watch = System.Diagnostics.Stopwatch.StartNew();
+            TestResult[]? currentResults = null;
 
-                var currentResults = base.Execute(testMethod);
+            while (begin_watch.ElapsedMilliseconds < stabilityThreshold * 1_000) {
+                string before_value = read_rapl_value();
+                long before_time = begin_watch.ElapsedTicks;
 
-                Decimal new_value = read_rapl_value();
-                watch.Stop();
-                //Thread.Sleep(1000);
-                foreach(var currentResult in currentResults) {
-                    RAPLTestResult current = new RAPLTestResult(new_value-before_value, currentResult);
-                    results.Add(current);
-                }
-                Console.WriteLine("{0};{1};{2};{3};{4}", count, new_value-before_value, before_value, new_value, watch.ElapsedTicks);
+                currentResults = base.Execute(testMethod);
 
-                
-                //Console.WriteLine("Iteration: {0} RAPL-Value: {1}, Old: {2}, New: {3}, Time: {4}", count, new_value-before_value, before_value, new_value, watch.ElapsedTicks);
+                string new_value = read_rapl_value();
+                long after_time = begin_watch.ElapsedTicks;
+
+                var energy_consumption = Decimal.Parse(new_value) - Decimal.Parse(before_value);
+                // Time in ticks.
+                var time_elapsed = after_time - before_time;
+                tuples.Add((energy_consumption, time_elapsed));
+
             }
-            var ticks_per_seconds = (decimal) 1 / Stopwatch.Frequency;
-            var ticks_per_milliseconds = (decimal) 1 / Stopwatch.Frequency * 1000;
-            var ticks_per_nanoseconds = (decimal) 1 / Stopwatch.Frequency * 1000000000;
+            
+            if(currentResults != null)
+                results.AddRange(currentResults);
+
+            
+            foreach (var tuple in tuples) {
+                System.Console.WriteLine(tuple.Item1 + ";" + tuple.Item2);
+            }
+            var ticks_per_seconds = (decimal)1 / Stopwatch.Frequency;
+            var ticks_per_milliseconds = (decimal)1 / Stopwatch.Frequency * 1000;
+            var ticks_per_nanoseconds = (decimal)1 / Stopwatch.Frequency * 1000000000;
             Console.WriteLine("Sec per tick: {0}, ms per tick: {1}, ns per tick: {2}", ticks_per_seconds, ticks_per_milliseconds, ticks_per_nanoseconds);
             System.Console.WriteLine("Frequency: {0}", System.Diagnostics.Stopwatch.Frequency);
             return results.ToArray();
--- FibLibraryTest/UnitTest1.cs
+++ FibLibraryTest/UnitTest1.cs
@@ -6,43 +6,15 @@ using UtilityLibraries;
 namespace StringLibraryTest;
 
 [TestClass]
-public class UnitTest1
-{
+public class UnitTest1 {
+
     [IterativeTestMethod(25)]
-    public void TestGetFib41()
-    {
-        Assert.AreEqual(FibLibrary.GetFib(40), 102334155);
-    }
-    [IterativeTestMethod(25)]
-    public void TestGetFib42()
-    {
-        Assert.AreEqual(FibLibrary.GetFib(41), 165580141);
-    }
-    [IterativeTestMethod(25)]
-    public void TestGetFib43()
-    {
+    public void TestGetFib42() {
         Assert.AreEqual(FibLibrary.GetFib(42), 267914296);
     }
+
     [IterativeTestMethod(25)]
-    public void TestSlowGetFib43()
-    {
+    public void TestSlowGetFib42() {
         Assert.AreEqual(FibLibrary.SlowGetFib(42), 267914296);
-    }
-
-    
-
-    [IterativeTestMethod(25)]
-    [DeploymentItem(@"test.csv", "optionalOutFolder")]
-    public void TestMathSqrt() {
-        using (TextFieldParser parser = new TextFieldParser("test.csv")) {
-            parser.TextFieldType = FieldType.Delimited;
-            parser.SetDelimiters(",");
-            while(!parser.EndOfData) {
-                string[] fields = parser.ReadFields();
-                var input_number = Double.Parse(fields[0]);
-                var output_number = Double.Parse(fields[1]);
-                Assert.AreEqual(output_number, Math.Sqrt(input_number));
-            }
-        }
     }
 }
