03067ae3e78c8810dda9ab0cb65da53f44a65f42
Jonas Andersen <jonas@anrsn.me>
2022-05-24 11:39:26 +0200

    Fixed MaxDepth for deserializing

--- json-fast-reverse/jsonnettest.cs
+++ json-fast-reverse/jsonnettest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace StringLibraryTest;
@@ -8,7 +9,7 @@ public class jsonnettest {
     [IterativeTestMethod(6)]
     public void TestNested1() {
         string nestedJsonText = (new string('[', 1)) + "1" + (new string(']', 1));
-        var nestedArray = JArray.Parse(nestedJsonText);
+        var nestedArray = (JArray) JsonConvert.DeserializeObject(nestedJsonText, new JsonSerializerSettings { MaxDepth = null});
         JToken elem = nestedArray;
         for (int i = 0; i < 1; i++) {
             elem = elem[0];
@@ -21,7 +22,7 @@ public class jsonnettest {
     [IterativeTestMethod(6)]
     public void TestNested100() {
         string nestedJsonText = (new string('[', 100)) + "1" + (new string(']', 100));
-        var nestedArray = JArray.Parse(nestedJsonText);
+        var nestedArray = (JArray) JsonConvert.DeserializeObject(nestedJsonText, new JsonSerializerSettings { MaxDepth = null});
         JToken elem = nestedArray;
         for (int i = 0; i < 100; i++) {
             elem = elem[0];
@@ -35,7 +36,7 @@ public class jsonnettest {
     [IterativeTestMethod(6)]
     public void TestNested10k() {
         string nestedJsonText = (new string('[', 10_000)) + "1" + (new string(']', 10_000));
-        var nestedArray = JArray.Parse(nestedJsonText);
+        var nestedArray = (JArray) JsonConvert.DeserializeObject(nestedJsonText, new JsonSerializerSettings { MaxDepth = null});
         JToken elem = nestedArray;
         for (int i = 0; i < 10_000; i++) {
             elem = elem[0];
